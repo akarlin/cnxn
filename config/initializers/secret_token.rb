2fe4ad3651ea8da1325ecf619eb385091bdc4d6f
Andrew Karlin <dkarlin@mycnxn.com>
2012-11-16 12:27:39 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cnxn::Application.config.secret_token = 'e0a433f1dfa6108f579cbe7c02866d1dbd94a9b9860ba0f1602e67bf92359a154a3a17c294e0d5548de4a7453af15c707d6af31cf3f3b9db37e1ea8c0ca527ad'
